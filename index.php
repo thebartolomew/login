ec5f370d719c002e86f41669624843c5e2fd75a1
thebartolomew <93366993+thebartolomew@users.noreply.github.com>
2021-11-17 22:15:09 +0600

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <title>Temirlan</title>
-<link rel="icon" href="https://mocah.org/uploads/posts/307049-Hina-Amano-Weathering-With-You-4K.jpg">
+<link rel="icon" href="https://mocah.org/uploads/posts/307049-Hina-Amano-Weathering-With-You-4K.png">
 </head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
